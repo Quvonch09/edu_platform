176a7a14d82a7c7e97d8a41bfe33decaecef23c6
asilbek8868 <asilbek8868@gmail.com>
2025-02-22 12:53:05 +0500

    graphic day update

--- src/main/java/com/example/edu_platform/repository/GraphicDayRepository.java
+++ src/main/java/com/example/edu_platform/repository/GraphicDayRepository.java
@@ -12,10 +12,10 @@ public interface GraphicDayRepository extends JpaRepository<GraphicDay, Long> {
     boolean existsByRoomIdAndStartTimeBeforeAndEndTimeAfter(Long roomId, LocalTime startTime,
                                                                       LocalTime endTime);
 
-    @Query(value = "select gd.* from room r join graphic_day gd on gd.id = r.graphic_day_id", nativeQuery = true)
+    @Query(value = "select gd.* from room r join graphic_day gd on gd.id = r.graphic_day_id limit 1", nativeQuery = true)
     Optional<GraphicDay> findByRoomId(Long roomId);
 
-    @Query(value = "select gd.* from graphic_day gd join groups g on gd.id = g.days_id where g.id = ?1", nativeQuery = true)
+    @Query(value = "select gd.* from graphic_day gd join groups g on gd.id = g.days_id where g.id = ?1 limit 1", nativeQuery = true)
     Optional<GraphicDay> findGraphicDay(Long id);
 
 
