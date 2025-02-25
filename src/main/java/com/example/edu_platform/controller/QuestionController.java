e973b355bfe459eeb15c7bb98f33b938012b5124
Khorezmian <sultanbaevm2001@gmail.com>
2025-02-25 18:28:21 +0500

    Chat

---
+++ src/main/java/com/example/edu_platform/controller/QuestionController.java
@@ -0,0 +1,56 @@
+package com.example.edu_platform.controller;
+
+import com.example.edu_platform.entity.enums.QuestionEnum;
+import com.example.edu_platform.payload.ApiResponse;
+import com.example.edu_platform.payload.req.ReqQuestion;
+import com.example.edu_platform.service.QuestionService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/question")
+public class QuestionController {
+    private final QuestionService questionService;
+
+    @PostMapping("/save")
+    @Operation(summary = "(TEACHER) Savol qo'shish")
+    @PreAuthorize("hasRole('ROLE_TEACHER')")
+    public ResponseEntity<ApiResponse> saveQuestion(
+            @RequestParam QuestionEnum questionEnum,
+            @RequestBody ReqQuestion reqQuestion
+    ){
+        return ResponseEntity.ok(questionService.saveQuestion(questionEnum, reqQuestion));
+    }
+
+    @GetMapping("/getByQuiz/{quizId}")
+    @Operation(summary = "Quizga tegishli questionlist ni olish")
+    public ResponseEntity<ApiResponse> getByQuiz(
+            @PathVariable Long quizId
+    ){
+        return ResponseEntity.ok(questionService.getQuestionByQuiz(quizId));
+    }
+
+    @PutMapping("/update/{questionId}")
+    @PreAuthorize("hasRole('ROLE_TEACHER')")
+    @Operation(summary = "(TEACHER) Questionni update qilish")
+    public ResponseEntity<ApiResponse> updateQuestion(
+            @PathVariable Long questionId,
+            @RequestParam QuestionEnum difficulty,
+            @RequestBody ReqQuestion reqQuestion
+    ){
+        return ResponseEntity.ok(questionService.updateQuestion(questionId, difficulty, reqQuestion));
+    }
+
+    @DeleteMapping("/delete/{questionId}")
+    @PreAuthorize("hasRole('ROLE_TEACHER')")
+    @Operation(summary = "(TEACHER) Question o'chirish")
+    public ResponseEntity<ApiResponse> deleteQuestion(
+            @PathVariable Long questionId
+    ){
+        return ResponseEntity.ok(questionService.deleteQuiz(questionId));
+    }
+}
---
+++ src/test/java/com/example/edu_platform/service/ModuleServiceTest.java
@@ -0,0 +1,98 @@
+package com.example.edu_platform.service;
+
+import com.example.edu_platform.entity.Category;
+import com.example.edu_platform.entity.Module;
+import com.example.edu_platform.payload.ApiResponse;
+import com.example.edu_platform.payload.ModuleDTO;
+import com.example.edu_platform.payload.req.ModuleRequest;
+import com.example.edu_platform.repository.CategoryRepository;
+import com.example.edu_platform.repository.ModuleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ModuleServiceTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private ModuleRepository moduleRepository;
+
+    @InjectMocks
+    private ModuleService moduleService;
+
+    private Module module;
+    private ModuleDTO moduleDTO;
+    private Category category;
+    private ModuleRequest moduleRequest;
+
+    @BeforeEach
+    void setUp(){
+        moduleRequest = new ModuleRequest();
+        moduleRequest.setName("Test Module");
+        moduleRequest.setCategoryId(1L);
+
+        category = new Category();
+        category.setId(1L);
+        category.setName("Test Category");
+        category.setDescription("Test Description");
+        category.setCoursePrice(100.0);
+        category.setDuration(30);
+        category.setActive(true);
+
+        module = new Module();
+        module.setId(1L);
+        module.setName("Test Module");
+        module.setCategory(category);
+        module.setDeleted(false);
+
+        moduleDTO = new ModuleDTO();
+        moduleDTO.setId(1L);
+        moduleDTO.setName("Test Module");
+        moduleDTO.setCategory("Test Category");
+        moduleDTO.setDeleted(false);
+    }
+
+    @Test
+    void saveModule_success(){
+        when(moduleRepository.existsByName(moduleRequest.getName())).thenReturn(false);
+        when(categoryRepository.findById(moduleRequest.getCategoryId())).thenReturn(Optional.ofNullable(category));
+        when(moduleRepository.save(any(Module.class))).thenReturn(module);
+
+        ApiResponse response = moduleService.createModule(moduleRequest);
+
+        assertNotNull(response);
+        assertNull(response.getError());
+        assertEquals("Modul yaratildi", response.getData());
+    }
+
+    @Test
+    void saveModule_alreadyExist(){
+        when(moduleRepository.existsByName(moduleRequest.getName())).thenReturn(true);
+        when(categoryRepository.findById(moduleRequest.getCategoryId())).thenReturn(Optional.ofNullable(category));
+
+        ApiResponse response = moduleService.createModule(moduleRequest);
+
+        assertNotNull(response.getError());
+        assertEquals("Modul allaqachon mavjud.",response.getError().getMessage());
+    }
+
+    @Test
+    void saveModule_categoryNotFound(){
+        when(categoryRepository.findById(moduleRequest.getCategoryId())).thenReturn(Optional.empty());
+
+        ApiResponse response = moduleService.createModule(moduleRequest);
+
+        assertNotNull(response.getError());
+        assertEquals("Kategoriya topilmadi.",response.getError().getMessage());
+    }
+}
