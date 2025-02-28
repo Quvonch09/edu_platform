a1ec0f325c10d21d46fe8134ab0ca2d33816366c
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2025-02-28 14:35:51 +0500

    hatoliklar tugrilandi

--- src/main/java/uz/sfera/edu_platform/entity/Lesson.java
+++ src/main/java/uz/sfera/edu_platform/entity/Lesson.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Entity
 public class Lesson extends AbsEntity {
 
+    @Column(nullable = false)
     private String name;
     private String description;
     private String videoLink;
