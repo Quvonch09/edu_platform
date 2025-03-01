85aa63d1d14bd42a297e739ffe8d533a575753b1
codot-09 <onabiyev626@gmail.com>
2025-03-01 14:43:20 -0800

    xatoliklar tuzatildi

--- src/main/java/uz/sfera/edu_platform/payload/req/ReqQuestion.java
+++ src/main/java/uz/sfera/edu_platform/payload/req/ReqQuestion.java
@@ -1,6 +1,7 @@
 package uz.sfera.edu_platform.payload.req;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,6 @@ public class ReqQuestion {
     @NotNull(message = "Quiz tanlanishi majburiy")
     private Long quizId;
 
-    @NotBlank(message = "Javoblar qo'shish majburiy")
+    @NotEmpty(message = "Javoblar qo'shish majburiy")
     private List<ReqOption> reqOptionList;
 }
