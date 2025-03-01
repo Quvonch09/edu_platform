25a90222bd18fd272fc8bd3aaf5bf2006e621783
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2025-03-01 14:23:35 +0500

    code optimizatsiya qilindi

--- src/main/java/uz/sfera/edu_platform/payload/req/ReqAdmin.java
+++ src/main/java/uz/sfera/edu_platform/payload/req/ReqAdmin.java
@@ -1,6 +1,7 @@
 package uz.sfera.edu_platform.payload.req;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
@@ -12,10 +13,16 @@ import lombok.*;
 public class ReqAdmin {
     @Schema(hidden = true)
     private Long id;
+
+    @NotBlank(message = "Bush bulmasin")
     private String fullName;
+
+    @NotBlank(message = "Bush bulmasin")
     @Pattern(regexp = "^998([0-9][012345789]|[0-9][125679]|7[01234569])[0-9]{7}$",
             message = "Invalid Uzbekistan phone number")
     private String phoneNumber;
+
+    @NotBlank(message = "Bush bulmasin")
     private String password;
     private Long fileId;
 }
