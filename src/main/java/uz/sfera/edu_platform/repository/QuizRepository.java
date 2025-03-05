d558346d23948d08f6517ff8dcb99d2f36eb5375
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2025-03-05 14:28:42 +0500

    xatolik tugrilandi

--- src/main/java/uz/sfera/edu_platform/repository/QuizRepository.java
+++ src/main/java/uz/sfera/edu_platform/repository/QuizRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuizRepository extends JpaRepository<Quiz,Long> {
     List<Quiz> findByLessonId(Long lessonId);
+    Optional<Quiz> findByIdAndDeleted(Long id, byte deleted);
 }
