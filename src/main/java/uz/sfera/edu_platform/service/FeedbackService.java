f2bb296848a25190371f33c994ec7573dc8ed0b8
codot-09 <onabiyev626@gmail.com>
2025-03-06 18:19:39 -0800

    Feedback tuzatildi

--- src/main/java/uz/sfera/edu_platform/service/FeedbackService.java
+++ src/main/java/uz/sfera/edu_platform/service/FeedbackService.java
@@ -137,12 +137,13 @@ public class FeedbackService {
 
         List<ResFeedback> resFeedbacks = new ArrayList<>();
         for (Long teacherId : teacherIds) {
+            User user = userRepository.findById(teacherId).get();
             ResFeedbackCount allByTeacher = feedbackRepository.findAllByTeacher(teacherId);
-            ResFeedbackCount allByLesson = feedbackRepository.findAllByLesson();
-            ResFeedbackCount allByQuiz = feedbackRepository.findAllByQuiz();
+            ResFeedbackCount allByLesson = feedbackRepository.findAllByLesson(teacherId);
+            ResFeedbackCount allByQuiz = feedbackRepository.findAllByQuiz(teacherId);
 
             ResFeedback resFeedback = ResFeedback.builder()
-                    .teacherName(allByTeacher.getTeacherName())
+                    .teacherName(user.getFullName())
                     .countLesson(allByLesson != null ? allByLesson.getFeedbackCount() :null)
                     .lessonBall(allByLesson != null ? allByLesson.getFeedbackBall() : null)
                     .quizCount(allByQuiz != null ? allByQuiz.getFeedbackCount() : null)
