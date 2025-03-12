f4198c4c886f42321f4decd582265ad3885913e3
codot-09 <onabiyev626@gmail.com>
2025-03-12 18:01:58 +0500

    tuzatildi

--- src/main/java/uz/sfera/edu_platform/controller/GroupController.java
+++ src/main/java/uz/sfera/edu_platform/controller/GroupController.java
@@ -32,9 +32,11 @@ public class GroupController {
     @Operation(summary = "Teacher o'ziga tegishli guruhlarni ko'rish")
     @PreAuthorize("hasRole('ROLE_TEACHER')")
     public ResponseEntity<ApiResponse> getByTeacher(
-            @CurrentUser User teacher
+            @CurrentUser User teacher,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
     ){
-        return ResponseEntity.ok(groupService.search(null,teacher.getFullName(),null,null,null, teacher.getId(), 0,10));
+        return ResponseEntity.ok(groupService.search(null,teacher.getFullName(),null,null,null, teacher.getId(), page,size));
     }
 
 
