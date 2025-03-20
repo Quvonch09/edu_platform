48416fa830ab2eca374dd294b9bceac592e3afeb
Mura <sultanbaevm2001@gmail.com>
2025-03-20 14:07:12 +0500

    chat group

--- src/main/java/uz/sfera/edu_platform/config/WebSocketConfig.java
+++ src/main/java/uz/sfera/edu_platform/config/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config)
     {
 
-        config.enableSimpleBroker("/user", "topic/group");
+        config.enableSimpleBroker("/user", "/topic");
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
     }
